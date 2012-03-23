6e343739fe190a97be2aa44a39fc67133d2d30e5
osa1 <omeragacan@gmail.com>
2012-03-23 08:35:25 +0200

    Func Show instance

--- Types.hs
+++ Types.hs
@@ -9,9 +9,15 @@ type Env = Map.Map String Val
 
 data Func = SForm (Params Sexp -> Env -> (Val, Env)) -- cond like forms
           | Func (Params Val -> Env -> (Val, Env))   -- ordinary functions, with optional side-effects(defun)
-          | SPure (Params Sexp -> Val)               -- macros?? quote like forms
+          | SPure (Params Sexp -> Val)               -- macros?? quote-like forms
           | Pure (Params Val -> Val)                 -- pure functions, car, cdr, etc
 
 data Params a = P1 a | P2 a a -- | P3 a a a
 
-data Val = VSymbol String | VList [Val] | VFunc Func
+data Val = VSymbol String | VList [Val] | VFunc Func deriving (Show)
+
+instance Show Func where
+    show (SForm _) = "SForm"
+    show (Func _)  = "Func"
+    show (SPure _) = "SPure"
+    show (Pure _)  = "Pure"
