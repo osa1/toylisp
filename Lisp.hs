2119000793b5ca74fc3ae244260158040193c745
osa1 <omeragacan@gmail.com>
2012-03-23 07:12:08 +0200

    function types and global environment

--- Lisp.hs
+++ Lisp.hs
@@ -3,10 +3,13 @@ module Lisp where
 import Types
 import Parser
 
+import qualified Data.Map as Map
 
-quote :: Sexp -> Val
-quote (List sexps) = VList $ map quote sexps
-quote (Atom symbol) = VSymbol symbol
+undefinedVal = VSymbol "undefined"
+
+quote :: Params Sexp -> Val
+quote (P1 (List sexps))  = VList $ map (quote . P1) sexps
+quote (P1 (Atom symbol)) = VSymbol symbol
 
 car :: Params Val -> Val
 car (P1 (VList (x:rest))) = x
@@ -18,3 +21,23 @@ cons :: Params Val -> Val
 cons (P2 v1 v2) = case v2 of
                     (VList lst) -> VList (v1:lst)
                     otherwise   -> VList [v1, v2]
+
+globalEnv = Map.fromList [ ("quote", SPure quote)
+                         , ("car", Pure car)
+                         , ("cdr", Pure cdr)
+                         , ("cons", Pure cons)
+                         ]
+
+eval :: Sexp -> Env -> (Val, Env)
+eval (List lst) env =
+    let f = eval (lst !! 0)
+        args = drop 1 lst
+    in apply f args env
+eval (Atom atom) env =
+    case Map.lookup atom env of
+        Just v -> (v, env)
+        Nothing -> (undefinedVal, env)
+
+
+apply = undefined
+
--- Types.hs
+++ Types.hs
@@ -5,13 +5,13 @@ import qualified Data.Map as Map
 -- syntax
 data Sexp = List [Sexp] | Atom String deriving (Show)
 
-type Env = Map.Map String Func
+type Env = Map.Map String Val
 
-data Func = SForm (Sexp -> Env -> (Val, Env)) -- cond like forms
-          | Func (Params Val -> Env -> (Val, Env)) -- ordinary functions, with optional side-effects(defun)
-          | SPure (Sexp -> Val)               -- macros?? quote like forms
+data Func = SForm (Params Sexp -> Env -> (Val, Env)) -- cond like forms
+          | Func (Params Val -> Env -> (Val, Env))   -- ordinary functions, with optional side-effects(defun)
+          | SPure (Params Sexp -> Val)               -- macros?? quote like forms
           | Pure (Params Val -> Val)                 -- pure functions, car, cdr, etc
 
 data Params a = P1 a | P2 a a | P3 a a a
 
-data Val = VSymbol String | VList [Val] deriving (Show)
+data Val = VSymbol String | VList [Val] | VFunc Func
