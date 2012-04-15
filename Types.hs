0ed967c10cbea7a56e773184a6f16fced23d299b
osa1 <omeragacan@gmail.com>
2012-04-15 13:16:56 +0300

    move unwordslist

--- Types.hs
+++ Types.hs
@@ -9,5 +9,15 @@ data LispVal = Atom String
              | Character Char
              | Float Float
              | Bool Bool
-    deriving (Show)
 
+instance Show LispVal where
+    show (String contents) = "\"" ++ contents ++ "\""
+    show (Atom name) = name
+    show (Number contents) = show contents
+    show (Bool True) = "#t"
+    show (Bool False) = "#f"
+    show (List contents) = "(" ++ unwordsList contents ++ ")"
+    show (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ show tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map show
