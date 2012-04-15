4469f6d8762c36718846e32c832a3d93568d3881
osa1 <omeragacan@gmail.com>
2012-04-15 13:16:17 +0300

    error types and helpers

---
+++ Error.hs
@@ -0,0 +1,41 @@
+module Error
+    ( LispError(..)
+    , ThrowsError
+    , trapError
+    , extractValue
+    , throwError
+    ) where
+
+import Types
+
+import Control.Monad.Error
+import Text.ParserCombinators.Parsec (ParseError(..))
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+instance Show LispError where
+    show (UnboundVar message varname) = message ++ ": " ++ varname
+    show (BadSpecialForm message form) = message ++ ": " ++ show form
+    show (NotFunction message func) = message ++ ": " ++ show func
+    show (NumArgs expected found) = "Expected " ++ show expected ++
+                                    " args: found values " ++ unwordsList found
+    show (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++
+                                         ", found " ++ show found
+    show (Parser parseErr) = "Parse error at " ++ show parseErr
+
+instance Error LispError where
+    noMsg = Default "An error has occured"
+    strMsg = Default
+
+type ThrowsError = Either LispError
+
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
