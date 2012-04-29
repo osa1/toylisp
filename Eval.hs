bb013621f0fa767b1435a9392068af58ff2452d6
osa1 <omeragacan@gmail.com>
2012-04-29 16:57:37 +0300

    variable handling (define/set! and getting values)

--- Eval.hs
+++ Eval.hs
@@ -3,6 +3,8 @@ module Eval where
 --import Control.Monad.Error
 
 import Control.Monad (liftM)
+import Data.IORef
+import Control.Monad.IO.Class (liftIO)
 
 import Error
 import Types
@@ -130,6 +132,44 @@ eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) &&
 eqv [_, _] = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
 
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+    env <- liftIO $ readIORef envRef
+    case lookup var env of
+        Just v  -> liftIO $ readIORef v
+        Nothing -> throwError $ UnboundVar "Getting an unbound variable" var
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var val = do
+    env <- liftIO $ readIORef envRef
+    case lookup var env of
+        Just v  -> liftIO $ writeIORef v val
+        Nothing -> throwError $ UnboundVar "Setting an unbound variable." var
+    return val
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var val = do
+    alreadyDefined <- liftIO $ isBound envRef var
+    if alreadyDefined
+        then setVar envRef var val >> return val
+        else liftIO $ do
+            valRef <- newIORef val
+            env <- readIORef envRef
+            writeIORef envRef ((var, valRef):env)
+            return val
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = do
+    env <- readIORef envRef
+    extEnv <- extendEnv bindings env
+    newIORef extEnv
+  where extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+        addBinding (var, val) = do
+            ref <- newIORef val
+            return (var, ref)
 
 eval :: LispVal -> ThrowsError LispVal
 eval (List [Atom "if", pred, conseq, alt]) = do
