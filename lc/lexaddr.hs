4b988e84cb7754f163ec9f1dc6e00588262d7bb2
osa1 <omeragacan@gmail.com>
2012-06-20 21:53:38 +0300

    let expressions

--- lc/lexaddr.hs
+++ lc/lexaddr.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GADTs, StandaloneDeriving #-}
 import Text.ParserCombinators.Parsec hiding (State(..))
-{-import Control.Monad.State-}
 import Control.Applicative ((<*))
 import Data.Set hiding (map)
 import Data.List (intercalate)
@@ -9,6 +8,7 @@ data Ident
 data Lambda
 data Application
 data If
+data Let
 
 type Var = String
 
@@ -17,12 +17,14 @@ data Expr a where
     If :: AnyExpr -> AnyExpr -> AnyExpr -> Expr If
     Lambda :: [Expr Ident] -> AnyExpr -> Expr Lambda
     Application :: [AnyExpr] -> Expr Application
+    Let :: [(Expr Ident, AnyExpr)] -> AnyExpr -> Expr Let
 
 instance Show (Expr a) where
     show (Ident id) = id
     show (If e1 e2 e3) = "(if " ++ (intercalate " " [show e1, show e2, show e3]) ++ ")"
     show (Lambda params expr) = "(lambda (" ++ (intercalate " " $ map show params) ++ ") " ++ show expr ++ ")"
     show (Application exprs) = "(" ++ (intercalate " " $ map show exprs) ++ ")"
+    show (Let exprs expr) = "let"
 
 data AnyExpr where
     AnyExpr :: Expr a -> AnyExpr
@@ -30,11 +32,9 @@ data AnyExpr where
 instance Show AnyExpr where
     show (AnyExpr e) = show e
 
-{-data CompilerState = CompilerState { indent :: Int }-}
 data CompilerState = CompilerState { indent :: Int }
 
 class Compilable a where
-    {-compile :: a -> State CompilerState String-}
     compile :: CompilerState -> a -> String
 
 indentedLine :: CompilerState -> String -> String
@@ -49,11 +49,15 @@ instance Compilable (Expr a) where
             indentedLine cs "else\n" ++ compile (CompilerState $ indent+2) elseE
 
     compile cs@(CompilerState indent) (Lambda args expr) =
-        indentedLine cs $ "fun " ++ (intercalate " " $ map (compile cs) args) ++ " ->\n" ++
+        indentedLine cs $ "fun " ++ (intercalate " " $ map (compile (CompilerState 0)) args) ++ " ->\n" ++
             compile (CompilerState $ indent+2) expr
 
     compile cs@(CompilerState indent) (Application exprs) =
-        indentedLine cs $ intercalate " " $ map (compile cs) exprs
+        indentedLine cs $ intercalate " " $ map (compile (CompilerState 0)) exprs
+
+    compile cs (Let defs expr) =
+        (intercalate "\n" $ map (\(v,d) -> "let " ++ show v ++ " = " ++ show d ++ " in") defs) ++
+            "\n" ++ compile cs expr
         
 
 instance Compilable AnyExpr where
@@ -97,17 +101,56 @@ ifExpr = do
     char ')'
     return $ If cond thenCase elseCase
 
+letExpr :: Parser (Expr Let)
+letExpr = do
+    char '('
+    wrapSpaces $ string "_let"
+    assigns <- assignment
+    body <- expr
+    char ')'
+    return $ Let assigns body
+  where assignment = do
+            char '('
+            assignments <- many1 $ wrapSpaces assign1
+            char ')'
+            return assignments
+        assign1 = do
+            char '('
+            var <- ident
+            def <- expr
+            char ')'
+            return (var, def)
+
 anyExpr :: Parser (Expr a) -> Parser AnyExpr
 anyExpr p = fmap AnyExpr p
 
 expr :: Parser AnyExpr
-expr = wrapSpaces $ choice [anyExpr $ try ident, anyExpr $ try lambda, anyExpr $ try ifExpr, anyExpr $ try application]
+expr = wrapSpaces $ choice [ anyExpr $ try ident
+                           , anyExpr $ try lambda
+                           , anyExpr $ try ifExpr
+                           , anyExpr $ try application
+                           , anyExpr $ try letExpr
+                           ]
 
 --------------------------------------------------------------------------------
 
-test = let st = parse expr "test" "(_if a b c)"
+test = let st = parse expr "test" "(_if a (_lambda (x) (add x foo)) (_lambda (y) (add y blah) (apply some fun)))"
        in case st of
              Right expr -> putStrLn $ compile (CompilerState 0) expr
              {-Right expr -> putStrLn $ show expr-}
              Left err -> putStrLn $ show err
 
+test2 = let st = parse expr "test2" "(_lambda (x y) (add x y))"
+        in case st of
+              Right expr -> putStrLn $ compile (CompilerState 0) expr
+              Left err -> putStrLn $ show err
+
+{-test3 = let st = parse expr "test3" "(_lambda (y) (add y blah) (apply some fun))"
+        in case st of
+              Right expr -> putStrLn $ compile (CompilerState 0) expr
+              Left err -> putStrLn $ show err-}
+
+test4 = let st = parse expr "test4" "(_let ((x foo) (y bar)) (add x y))"
+        in case st of
+              Right expr -> putStrLn $ compile (CompilerState 0) expr
+              Left err -> putStrLn $ show err
