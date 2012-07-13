88295afcb1dd571bf509b7cc9a05769a6e9e8302
osa1 <omeragacan@gmail.com>
2012-07-13 21:29:41 +0300

    IO primitives

--- IO.hs
+++ IO.hs
@@ -1,3 +1,11 @@
 -- IO functions
 
 module IO where
+
+import Types
+import Data.List (intercalate)
+import Control.Monad.IO.Class (liftIO)
+
+println :: SimpleFunc
+println args = liftIO (putStrLn $ intercalate "    " (map show args)) >> return Nil
+
